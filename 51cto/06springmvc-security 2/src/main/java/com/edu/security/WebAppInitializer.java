d47ac0ec03a0fd9831bd5dd08a22026201bc8394
jun.chu <1571970439@qq.com>
2017-07-31 16:20:20 +0800

    add 51cto-06

---
+++ 51cto/06springmvc-security 2/src/main/java/com/edu/security/WebAppInitializer.java
@@ -0,0 +1,24 @@
+package com.edu.security;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ *  系统初始化类
+ */
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[]{WebAppConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[]{"/"};
+	}
+}
---
+++ 51cto/06springmvc-security 2/src/main/java/com/edu/security/controller/LoginController.java
@@ -0,0 +1,35 @@
+package com.edu.security.controller;
+
+import org.springframework.security.authentication.AccountExpiredException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/sys/login")
+	public String login(){
+		return "/jsp/login";
+	}
+	
+	@PostMapping("/sys/loginFail")
+	public String fail(HttpServletRequest req){
+		//获取spring security权限验证失败原因
+		AuthenticationException exp = (AuthenticationException)req.getAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+		if(exp instanceof BadCredentialsException){
+			req.setAttribute("error_msg", "用户名或密码错误");
+		} else if(exp instanceof AccountExpiredException){
+			req.setAttribute("error_msg", "账户过期");
+		} else if(exp instanceof LockedException){
+			req.setAttribute("error_msg", "账户已被锁");
+		}
+		return "/jsp/login";
+	}
+}
